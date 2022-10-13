d54b7745416746e1fc5edbaec3dbf7cd933ed853
Kim <kim@MacBook-Pro-de-Kim.local>
2022-10-13 00:17:23 -0300

    Actualizado

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+        <h2>My weather App</h2>
+        <p>
+            En esta app vas a poder ver el clima en distintas ciudades del mundo
+        </p>
+        <footer>Nicolas Kim</footer>
+    </div>
+  );
+}
--- 09-React-Routing/homework/src/containers/App.js
+++ 09-React-Routing/homework/src/containers/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-
+import { BrowserRouter, Router } from 'react-router-dom';
 import './App.css';
 import Nav from '../components/Nav.jsx';
 import Cards from '../components/Cards.jsx';
+import About from '../components/About';
+import { Route,Switch } from 'react-router-dom';
+import Ciudad from '../components/Ciudad'
 
-const apiKey = 'Aqui va la API key que creaste';
+const apiKey = '070d5956b3be7bc9696e1ef3b683db79';
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -44,18 +47,38 @@ function App() {
         return null;
     }
   }
+function onFilter(ciudadId){
+  let ciudad = cities.find(c=>c.id === parseInt(ciudadId))
+  return ciudad
+}
+
   return (
-    <div className="App">
-      <Nav onSearch={onSearch}/>
-      <div>
-        <Cards
-          cities={cities}
-          onClose={onClose}
-        />
-      </div>
-      <hr />
+    <div className="App">    
+     <Nav onSearch={onSearch}/>
+   
+          <Switch>
+            <Route exact path="/">
+              <Cards
+                cities={cities}
+                onClose={onClose}
+              />
+            </Route>
+            <Route path="/about">
+              <About/>
+            </Route>
+            <Route path={`/ciudad/:id`}
+            render={ ( {match} ) => <Ciudad city={onFilter(match.params.id)}/>}
+            >
+              
+              
+            </Route>
+          </Switch>
+ 
+    
     </div>
   );
 }
 
 export default App;
+
+
